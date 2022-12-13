cc4e91b43f8672eec5921f8b07c4bff32b0f145e
jangseoyoung98 <77472570+jangseoyoung98@users.noreply.github.com>
2022-12-13 22:27:24 +0900

    22-2 퇴사근무표 작성 (not completed)
    - string 배열로 해서 랜덤하게 뽑을 예정
    - 월 / 화 / 수 / 목 자료구조 어떻게..? (2차원 배열?)

---
+++ C++/time.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+// 총 20명 (a ~ r) -> 이름 가나다 순  ※ 이성현, 송민규 -> 비상 대기
+// 기존 13 : 강기윤, 김서연, 김지현, 박소연, 박채빈, (소)찬호, 송민규, 이경원, 장서영, 조인철, (주)수현, 천재현, (최)여원 
+// 신입 6 : 안준호, 장윤혜, 정유진, 정윤주, 황보효인, 최희승
+
+int 강기윤, 김서연, 김지현, 박소연, 박채빈, 소찬호, 송민규, 이경원, 장서영, 조인철, 주수현, 천재현, 최여원;
+int 안준호, 장윤혜, 정유진, 정윤주, 황보효인, 최희승;
+
+// 1명씩
+string 자유[3][10] = {};
+string 정의[3][10] = {};
+// 2명씩
+string 진리_여[3][10] = {};
+string 진리_남[3][10] = {};
+string 미래_여[3][10] = {};
+string 미래_남[3][10] = {};
+
+// 19일 (임시)
+void Monday() {
+	string 열시[] = { "김서연", "박채빈", "소찬호", "박소연", "이경원", "정유진", "주수현", "최여원", "황보효인", "장서영" };
+	string 열한시[] = {  };
+	string 열두시[] = {  };
+	string eleven[] = {  };
+	string eleven[] = {  };
+	string eleven[] = {  };
+	string eleven[] = {  };
+	string eleven[] = {  };
+	string eleven[] = {  };
+	string eleven[] = {  };
+
+	// https://kin.naver.com/qna/detail.nhn?d1id=1&dirId=1040101&docId=303730404&qb=6rCI67mE7LSI67Cl&enc=utf8&section=kin.qna&rank=17&search_sort=0&spq=0
+
+}
+
+// 20일 ~ 21일
+void Tuesday_to_Wednseday() {
+
+}
+
+// 22일 (3시간 근무)
+void Thursday() {
+
+}
+
+int main() {
+
+
+	return 0;
+}
